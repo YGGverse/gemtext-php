207f785d7f4e425af0f165c35afd4fd8f06071c7
yggverse <yggverse@project>
2024-06-23 19:22:56 +0300

    create Entity namespace

---
+++ src/Entity/Line.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yggverse\Gemtext\Entity;
+
+class Line
+{
+    private string $_data;
+
+    private bool $_escaped;
+    private ?int $_number;
+
+    public function __construct(
+        string $data = '',
+        bool $escaped = false,
+        ?int $number = null
+    ) {
+        $this->setData(
+            $data
+        );
+
+        $this->setEscaped(
+            $escaped
+        );
+
+        $this->setNumber(
+            $number
+        );
+    }
+
+    public function getData(): string
+    {
+        return $this->_data;
+    }
+
+    public function setData(
+        string $data
+    ): void
+    {
+        $this->_data = $data;
+    }
+
+    public function getEscaped(): bool
+    {
+        return $this->_escaped;
+    }
+
+    public function setEscaped(
+        bool $escaped
+    ): void
+    {
+        $this->_escaped = $escaped;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->_number;
+    }
+
+    public function setNumber(
+        ?int $number
+    ): void
+    {
+        $this->_number = $number;
+    }
+}
